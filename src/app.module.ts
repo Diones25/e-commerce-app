c63b39b2a6e4da6aead46280b5e94aa4578279f0
Diones <pereiradiones987@gmail.com>
2026-02-07 23:02:48 -0300

    configurando variáveis de ambiente no modulo TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 // todo: adicionar o usuário e senha do banco de dados em variáveis de ambiente 
@@ -8,15 +8,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database: 'ecommerce_db',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async () => {
+        const configService = new ConfigService();
+        return {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: parseInt(configService.get('POSTGRES_PORT') || '5432'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DATABASE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+          migrations: [__dirname + '/migrations/*{.ts,.js}'],
+          cli: {
+            migrationsDir: 'src/migrations',
+          },
+        }
+      }
     }),
   ],
   controllers: [],
