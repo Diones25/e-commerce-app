f3982afb5e5ab561103474e1e44d4fef721efc21
Diones <pereiradiones987@gmail.com>
2026-02-07 22:46:47 -0300

    configurando typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// todo: adicionar o usuário e senha do banco de dados em variáveis de ambiente 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'ecommerce_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
