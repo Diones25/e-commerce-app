9dade25612bcba7cca630fad806a90834196920b
Diones <pereiradiones987@gmail.com>
2026-02-08 18:58:17 -0300

    Entidade de produtos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,33 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('products')
+export class Product {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('decimal', { default: 0.0 })
+  price: number;
+
+  @Column('text', { default: 'no-image.png' })
+  image: string;
+
+  @Column('simple-json', {
+    default: {},
+    comment: "a key-value pair that represents the product's specs",
+  })
+  specs: Record<string, any>;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+  
+}
