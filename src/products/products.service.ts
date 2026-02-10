2b45f90320d6576b39b8b6451adb7ee65d7f8445
Diones <pereiradiones987@gmail.com>
2026-02-10 17:27:50 -0300

    criando e retornando produtos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,22 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = this.productsRepository.create(createProductDto);
+    await this.productsRepository.save(product);
+    return {
+      message: 'Product created successfully',
+      product,
+    }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(page = 1, limit = 10) {
+    const products = await this.productsRepository.find({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+    return {
+      products,
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productsRepository.findOne({
+      where: { id },
+    });
+    return {
+      product,
+    };
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
