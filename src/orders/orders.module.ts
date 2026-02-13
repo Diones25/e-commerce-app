8fccd579f3c63ea22eb6465c30c222663656c99d
Diones <pereiradiones987@gmail.com>
2026-02-13 13:45:50 -0300

    importante repositories

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,10 +3,11 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, Product])],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
