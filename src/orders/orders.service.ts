26887eddf182044485dd640cf5a0a1b0c8d0fdef
Diones <pereiradiones987@gmail.com>
2026-02-13 13:43:32 -0300

    importante repositories

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class OrdersService {
+
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
