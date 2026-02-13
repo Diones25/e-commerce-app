c72bf5c6130a3bbb9cab8200c1610f2559ece772
Diones <pereiradiones987@gmail.com>
2026-02-13 13:56:02 -0300

    adicionando dto de orders

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,26 @@
-export class CreateOrderDto {}
+import { IsDecimal, IsPositive, IsString, IsUUID } from "class-validator";
+
+export class CreateOrderDto {
+
+  @IsString({ message: 'customerId deve ser uma string' })
+  @IsUUID('all', { message: 'customerId deve ser um UUID válido.' })
+  customerId: string;
+
+  @IsString({ message: 'productId must be a string' })
+  @IsUUID('all', { message: 'productId must be a valid UUID' })
+  productId: string;  
+
+  @IsPositive({
+    message: 'quantity must be a positive number'
+  })
+  quantity: number;
+  @IsDecimal(
+    {
+      decimal_digits: '2'
+    },
+    {
+      message: 'totalPrice must be a decimal number'
+    },
+  )
+  totalPrice: number;
+}
