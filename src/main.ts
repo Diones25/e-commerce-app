ba6ee6c1e4ed7858c6bc21cd8fee4638128609c9
Diones <pereiradiones987@gmail.com>
2026-04-19 17:40:06 -0300

    feat: implementando filtro personalizado de exeção

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { APIResponseInterceptor } from './common/interceptors/api-response-interceptor';
+import { HttpExceptionFilter } from './common/exceptions-filters/http- exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new APIResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
