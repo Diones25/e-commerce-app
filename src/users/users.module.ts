02a12811de154f5ba91ab271c9cf55725ec166b0
Diones <pereiradiones987@gmail.com>
2026-04-19 22:41:08 -0300

    feat: atualizando modulo de usuários

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,15 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from 'src/common/common.module';
+import { PaginationService } from 'src/common/pagination/pagination.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    CommonModule
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PaginationService],
 })
 export class UsersModule {}
