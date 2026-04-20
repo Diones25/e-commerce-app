b48ffcd1e34409b070345400fb76c8ca50249462
Diones <pereiradiones987@gmail.com>
2026-04-19 22:02:25 -0300

    feat: dto de usuários

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { IsString, IsStrongPassword, Length } from "class-validator";
+
+export class CreateUserDto {
+  @IsString({ message: 'O nome de usuário deve ser uma string' })
+  username: string;
+
+  @IsString({ message: 'A senha deve ser uma string' })
+  @Length(8, 20)
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    }, 
+    {
+      message: 'A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um número e um símbolo'
+    }
+  )
+  password: string;
+
+  @IsString({ message: 'O email deve ser uma string' })
+  email: string;
+}
