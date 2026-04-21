3efce433267b9381e1243c8337941262d483a102
Diones <pereiradiones987@gmail.com>
2026-04-20 21:27:40 -0300

    feat: atualizando controller de autenticação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,2 +1,8 @@
 
-export class AuthController {}
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+}
