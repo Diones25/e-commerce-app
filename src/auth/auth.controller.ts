a9e8cdd4bc3f259f13ccc70716fe8258b1a042c0
Diones <pereiradiones987@gmail.com>
2026-04-26 14:18:32 -0300

    feat: atualizando controller de autenticação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,22 @@
 
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+  
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    const { username, password } = loginUserDto;
+    return await this.authService.login(username, password);
+  }
 }
